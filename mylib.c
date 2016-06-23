c63e8cc34e05b814955b44027292592894a79f05
吴胜军 <wushengjun85@163.com>
2016-06-23 09:15:27 +0800

    Add files via upload

---
+++ mylib.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+ > File Name: mylib.c
+ > Author: wsj
+ > Mail: wushengjun85@163.com 
+ > Created Time: 2016年06月22日 星期三 16时22分52秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include "mylib.h"
+void P()
+{
+
+	printf("Hello World\r\n");
+	printf("Hello fun so\r\n");
+}
